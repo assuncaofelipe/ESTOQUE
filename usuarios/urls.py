5ce2d1db9eb843e2e79c174c41fe414e10e685c1
Felippe Assunção <fassuncao368@gmail.com>
2022-04-21 22:02:38 -0400

    :package: usuarios

---
+++ usuarios/urls.py
@@ -0,0 +1,9 @@
+from re import template
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    #path('', view, name=""),
+    path('login/', auth_views.LoginView.as_view( template_name = 'usuarios/form_user.html' ), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
